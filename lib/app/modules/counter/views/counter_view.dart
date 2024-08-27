3de80d03489b132b23121dec51983322af1606cb
kytonas <faikarksatria@gmail.com>
2024-08-27 11:39:11 +0700

    biodata-flutter

--- lib/app/modules/counter/views/counter_view.dart
+++ lib/app/modules/counter/views/counter_view.dart
@@ -5,8 +5,8 @@ import 'package:get/get.dart';
 import '../controllers/counter_controller.dart';
 
 class CounterView extends GetView<CounterController> {
-  
-  CounterView({super.key});
+  const CounterView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +16,41 @@ class CounterView extends GetView<CounterController> {
       ),
       body: Center(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Hitungan ke-"),
-              Obx(
-                () => Text(controller.bilangan.toString()),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  controller.tambahSatu();
-                },
-                child: Icon(Icons.add),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  controller.kurangSatu();
-                },
-                child: Icon(Icons.remove),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  controller.reset();
-                },
-                child: Icon(Icons.refresh),
-              ),
-            ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hitungan ke : "),
+            Obx(
+              () => Text(controller.bilangan.toString()),
+            ),
+            const SizedBox(height: 10), // Add spacing between text and buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    controller.tambahSatu();
+                  },
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(width: 10), // Add spacing between buttons
+                ElevatedButton(
+                  onPressed: () {
+                    controller.kurangSatu();
+                  },
+                  child: Icon(Icons.remove),
+                ),
+                SizedBox(width: 10), // Add spacing between buttons
+                ElevatedButton(
+                  onPressed: () {
+                    controller.reset();
+                  },
+                  child: Icon(Icons.refresh),
+                ),
+              ],
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,6 +6,7 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,11 +22,26 @@ class HomeView extends GetView<HomeController> {
               'HomeView is working',
               style: TextStyle(fontSize: 20),
             ),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/counter');
-                },
-                child: Text('Counter Page')),
+            const SizedBox(
+                height: 16), // Menambahkan jarak antara teks dan tombol
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/counter');
+                  },
+                  child: Text('Counter Page'),
+                ),
+                const SizedBox(width: 16), // Menambahkan jarak antar tombol
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/biodata');
+                  },
+                  child: Text('Biodata Page'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
